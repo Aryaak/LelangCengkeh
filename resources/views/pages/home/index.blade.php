38221f67181210147257f3894a63f50421730633
= <=>
2022-04-02 14:03:13 +0700

    Translate

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="container">
         <h1 class="mb-3 fw-bold">Lelang berlangsung</h1>
-
     <div class="row">
         @forelse ($data as $item)
             <div class="col-md-3">
