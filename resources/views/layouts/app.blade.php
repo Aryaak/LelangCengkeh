0baf28160784f97f17c1780c7d61be5667ca633d
= <=>
2022-04-04 12:20:36 +0700

    Update info

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,8 +32,15 @@
     <main class="py-4">
         @yield('content')
     </main>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script>
+        setInterval(() => {
+            window.location.href = '/'
+            console.log('reload')
+        }, 3000);
+    </script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuctionController;
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth']], function () {
