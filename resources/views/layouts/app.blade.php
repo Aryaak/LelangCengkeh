79ba64522c7f250265fdf0872ec86c54bdff5a87
= <=>
2022-04-04 13:00:24 +0700

    Update info

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,12 +32,7 @@
     <main class="py-4">
         @yield('content')
     </main>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <script>
-        window.location.reload(true)
-    </script>
 </body>
 
 </html>
