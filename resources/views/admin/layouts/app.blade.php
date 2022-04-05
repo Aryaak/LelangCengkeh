46c79e95838132b4dbef37e472d86f76441dc417
= <=>
2022-04-05 07:52:48 +0700

    Fix modal info

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -29,6 +29,7 @@
     <link rel="stylesheet" href="{{asset('plugins/daterangepicker/daterangepicker.css')}}">
     <!-- summernote -->
     <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
+    <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
