86868b0d4314f4ab48761b10b8e55821201bb6c9
= <=>
2022-04-05 11:06:57 +0700

    Fix modal info

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,33 +1,41 @@
 {{-- <nav class="navbar navbar-light bg-primary ">
   <div class="container">
     <a class="navbar-brand fw-bolder text-light" href="{{route('home')}}">
-      {{ config('', 'Cengkeh') }}
-    </a>
-  </div>
+{{ config('', 'Cengkeh') }}
+</a>
+</div>
 </nav> --}}
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3 bg-body rounded">
     <div class="container-fluid">
-      <a class="navbar-brand fw-bold fs-2" onclick="window.location.reload(true)">{{ config('app.name', 'Laravel') }}</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item mt-3">
-            <form action="{{route('logout')}}" method="POST">
-              @csrf
-              <input type="hidden" name="email" value="{{Auth::user()->email}}">
-              <button class="btn btn-danger w-100" >Keluar</button>
-            </form>
-          </li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
+        <form action="{{ route('search') }}" method="get">
+            @csrf
+            <input name="search" type="hidden" value="" class="form-control w-25" aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-sm" style="outline: 0px !important; box-shadow:none;">
+            <button type="submit" style="border: none" class="navbar-brand fw-bold fs-2  bg-transparent "
+                onclick="window.location.reload(true)">{{ config('app.name', 'Laravel') }}</button>
         </form>
-          {{-- <li class="nav-item">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item mt-3">
+                    <form action="{{route('logout')}}" method="POST">
+                        @csrf
+                        <input type="hidden" name="email" value="{{Auth::user()->email}}">
+                        <button class="btn btn-danger w-100">Keluar</button>
+                    </form>
+                </li>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+                {{-- <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
           </li> --}}
-          {{-- <li class="nav-item dropdown">
+                {{-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
@@ -46,6 +54,6 @@
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form> --}}
-      </div>
+        </div>
     </div>
-  </nav>
+</nav>
