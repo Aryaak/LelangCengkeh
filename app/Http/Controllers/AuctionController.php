644581a61321a3ebaa1ecfac367e4a2cd88960dc
muhBilal <mb336450@gmail.com>
2022-04-06 20:57:24 +0700

    fix error

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -86,7 +86,6 @@ class AuctionController extends Controller
 
     public function store(Request $request)
     {
-        dd(request()->all());
         $data = $this->validate($request, [
             'photo' => 'required',
             'title' => 'required',
